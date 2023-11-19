81c7747a2c109f38d721988bd10dbf60feb4f7a6
胡鹏 <hupgwuty@163.com>
2023-11-19 16:15:49 +0800

    摸鱼组件基础时间配置

--- src/components/MigrantWorkers.vue
+++ src/components/MigrantWorkers.vue
@@ -1,13 +1,106 @@
 <template>
-    <div>
-      MigrantWorkers
+  <div class="main_container">
+    <div class="title">摸鱼时间到</div>
+    <el-divider class="divider" content-position="left">时间配置</el-divider>
+    <div class="form_block">
+      <span>工作时间</span>
+      <el-time-select
+        v-model="workTimeStart"
+        :max-time="workTimeEnd"
+        class="time-select"
+        placeholder="开始时间"
+        start="00:00"
+        step="00:15"
+        end="23:45"
+        default-value="new Date()"
+        is-range
+      />
+      <el-time-select
+        v-model="workTimeEnd"
+        :min-time="workTimeStart"
+        class="time-select"
+        placeholder="结束时间"
+        start="00:00"
+        step="00:15"
+        end="23:45"
+        is-range
+      />
     </div>
-  </template>
+    <div class="form_block">
+      <span>午休时间</span>
+      <el-time-select
+        v-model="restTimeStart"
+        :max-time="restTimeEnd"
+        class="time-select"
+        placeholder="开始时间"
+        start="00:00"
+        step="00:15"
+        end="23:45"
+        default-value="new Date()"
+        is-range
+      />
+      <el-time-select
+        v-model="restTimeEnd"
+        :min-time="restTimeStart"
+        class="time-select"
+        placeholder="结束时间"
+        start="00:00"
+        step="00:15"
+        end="23:45"
+        is-range
+      />
+    </div>
+  </div>
+</template>
   
-  <script>
+<script>
+import { ref } from 'vue'
   export default {
-    name: 'MigrantWorkers'
+    name: 'MigrantWorkers',
+    setup() {
+      const workTimeStart = ref('09:00');
+      const workTimeEnd = ref('18:00');
+      const restTimeStart = ref('12:00')
+      const restTimeEnd = ref('13:00')
+
+      return {
+        workTimeStart,
+        workTimeEnd,
+        restTimeStart,
+        restTimeEnd
+      };
+    }
   }
-  </script>
-  <style scoped>
-  </style>
+</script>
+<style scoped>
+.main_container {
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.title {
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+  color: rgb(0, 47, 167);
+}
+
+.divider {
+  border-width: 2px;
+  border-color: skyblue;
+}
+
+span {
+  margin-right: 10px;
+}
+
+.time-select {
+  margin-right: 10px;
+}
+
+.form_block{
+  padding-left: 10px;
+  margin-bottom: 10px;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount('#app')
+//引入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+app.mount('#app')
